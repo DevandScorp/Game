ef5f61d539eceb700782917e8dd9f2ddfb2edf23
DevandScorp <artemda459@gmail.com>
2018-12-10 13:12:19 +0300

    feat: edit health bars

--- src/screen/game/js/changeHealthBar.js
+++ src/screen/game/js/changeHealthBar.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
 export default function moveHealth(npcElem, value) {
-  npcElem.style.width = `${value / 2}%`;
+  npcElem.style.width = `${value}%`;
   npcElem.innerHTML = `${value * 1}%`;
 }
